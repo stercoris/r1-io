0bb88658ca272ca2b72990ba71ccc1e497add3b6
Dmitriy Rodin <dimarodin0@gmail.com>
2021-10-31 22:47:20 +0400

    build(packages): trying to move from promise-resolve-deep to promise-props-recursive

--- src/unpacker/getDeepResolved.ts
+++ src/unpacker/getDeepResolved.ts
@@ -1,12 +1,11 @@
 // this is some unsafe shit, but no other way :sadface:
 
 import { R1Node } from "@Factory/factory";
-
 //@ts-ignore
-require("promise-resolve-deep")(Promise);
+import { promisePropsRecursive } from "promise-props-recursive";
 
 export const deepResolveContent = async (
   objectWithPromiseChain: R1Node
 ): Promise<R1Node> =>
   //@ts-ignore
-  Promise.resolveDeep(objectWithPromiseChain);
+  promisePropsRecursive(objectWithPromiseChain)[0];
