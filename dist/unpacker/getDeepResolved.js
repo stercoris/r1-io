8b66ec343fe019bf871242fce1e076ac19256354
Dmitriy Rodin <dimarodin0@gmail.com>
2021-10-31 22:48:17 +0400

    build

--- dist/unpacker/getDeepResolved.js
+++ dist/unpacker/getDeepResolved.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deepResolveContent = void 0;
-require("promise-resolve-deep")(Promise);
-const deepResolveContent = async (objectWithPromiseChain) => Promise.resolveDeep(objectWithPromiseChain);
+const promise_props_recursive_1 = require("promise-props-recursive");
+const deepResolveContent = async (objectWithPromiseChain) => (0, promise_props_recursive_1.promisePropsRecursive)(objectWithPromiseChain)[0];
 exports.deepResolveContent = deepResolveContent;
 //# sourceMappingURL=getDeepResolved.js.map
