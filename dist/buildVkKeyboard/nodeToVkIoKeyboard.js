4f0a896e8e06a61217f590c9c8d672ed1347dcab
Dmitriy Rodin <dimarodin0@gmail.com>
2021-11-01 19:37:02 +0400

    feat(node to vk-io keyboard): added additional checks

--- dist/buildVkKeyboard/nodeToVkIoKeyboard.js
+++ dist/buildVkKeyboard/nodeToVkIoKeyboard.js
@@ -2,7 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.nodeToVkIoKeyboard = void 0;
 const vk_io_1 = require("vk-io");
-const getRow = (node) => node.content.map((n) => vk_io_1.Keyboard.textButton(n.content));
+const getButton = (node) => {
+    if (node.type !== "button") {
+        throw new Error("Only objects with type 'button' should be parsed to VKButton");
+    }
+    return vk_io_1.Keyboard.textButton(node.content);
+};
+const getRow = (node) => {
+    if (node.type !== "row") {
+        throw new Error("Only objects with type 'row' should be parsed to VKRow");
+    }
+    return node.content.map(getButton);
+};
 const nodeToVkIoKeyboard = (node) => {
     if (node.type !== "menu") {
         throw new Error("Only menu should be parsed to vk-io keyboard");
--- src/buildVkKeyboard/nodeToVkIoKeyboard.ts
+++ src/buildVkKeyboard/nodeToVkIoKeyboard.ts
@@ -1,10 +1,21 @@
 import { R1Node } from "@Factory/factory";
 import { IKeyboardProxyButton, Keyboard, KeyboardBuilder } from "vk-io";
 
-const getRow = (node: R1Node): IKeyboardProxyButton[] =>
-  (node.content as R1Node[]).map((n) =>
-    Keyboard.textButton(n.content as JSX.ButtonPayload)
-  );
+const getButton = (node: R1Node): IKeyboardProxyButton => {
+  if (node.type !== "button") {
+    throw new Error(
+      "Only objects with type 'button' should be parsed to VKButton"
+    );
+  }
+  return Keyboard.textButton(node.content as JSX.ButtonPayload);
+};
+
+const getRow = (node: R1Node): IKeyboardProxyButton[] => {
+  if (node.type !== "row") {
+    throw new Error("Only objects with type 'row' should be parsed to VKRow");
+  }
+  return (node.content as R1Node[]).map(getButton);
+};
 
 export const nodeToVkIoKeyboard = (node: R1Node): KeyboardBuilder => {
   if (node.type !== "menu") {
