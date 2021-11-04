b57a51c9ee22732d9edc36fe02e937fed6567b2c
Dmitriy Rodin <dimarodin0@gmail.com>
2021-11-04 16:05:32 +0400

    feat(package.jsnon): added promise-resolve-deep-ts library & updated typescript version

--- src/unpacker/unpack.ts
+++ src/unpacker/unpack.ts
@@ -1,6 +1,6 @@
 import { R1Node } from "@Factory/factory";
 import { deleteFunctionalComponents } from "@Unpacker/deleteFunctionalComponents";
-import { deepResolveContent } from "@Unpacker/getDeepResolved";
+import { promiseDeepResolve } from "promise-deep-resolve-ts";
 
 export const unpackContent = async (menu: R1Node): Promise<R1Node> =>
-  deleteFunctionalComponents(await deepResolveContent(menu));
+  deleteFunctionalComponents(await promiseDeepResolve(menu));
