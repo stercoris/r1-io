9d6baaf229dbcd57cc1ed2dd45b05280fb2170dc
Dmitriy Rodin <dimarodin0@gmail.com>
2021-12-12 22:59:56 +0400

    tests(actions): tests for actions creators

---
+++ src/action/createAction/__test__/createAction.test.ts
@@ -0,0 +1,57 @@
+import {Actions, SimpleAction} from '@Root';
+import {MessageContext} from 'vk-io';
+import {createAction} from '../createAction';
+
+const TEST_ACTION_NAME = 'test action name';
+
+interface GlobalContext {
+  secondHello: string;
+}
+
+describe('Create Simple Action', () => {
+  const actionExecutorMock = jest.fn();
+
+  const testAction = createAction<GlobalContext>(
+    TEST_ACTION_NAME,
+    actionExecutorMock
+  );
+
+  const actionPayload = testAction();
+
+  test('Action constructor should have correct props', async () => {
+    expect(actionPayload.name).toBe('test action name');
+  });
+
+  test('Action should be in ActionsBuffer', () => {
+    expect(Actions.isActionAlreadyExists(TEST_ACTION_NAME)).toBeTruthy();
+  });
+
+  test('Action executor should have correct props', async () => {
+    const actionExecutor = Actions.find(TEST_ACTION_NAME);
+
+    if (!actionExecutor) {
+      throw new Error(`There is no "${TEST_ACTION_NAME}" action`);
+    }
+
+    expect(actionExecutor.type).toBe('SimpleAction');
+    expect(actionExecutor.name).toBe(TEST_ACTION_NAME);
+
+    expect(typeof actionExecutor.do === typeof Function).toBe(true);
+
+    await (actionExecutor.do as SimpleAction<any>)(
+      {test: 1} as unknown as MessageContext,
+      {test: 2}
+    );
+
+    expect(actionExecutorMock).toHaveBeenCalledWith({test: 1}, {test: 2});
+  });
+
+  test('Dublicate action creation should throw an "This action already exists" error', () => {
+    const createDublicateAction = () =>
+      createAction(TEST_ACTION_NAME, () => {});
+
+    expect(createDublicateAction).toThrowErrorMatchingInlineSnapshot(
+      `"Action with name \\"test action name\\" already exist"`
+    );
+  });
+});
---
+++ src/action/createParameterizedAction/__test__/createParameterizedAtion.spec.ts
@@ -0,0 +1,73 @@
+import {Actions, createAction, createParametarizedAction} from '@Root';
+import {MessageContext} from 'vk-io';
+
+const TEST_ACTION_NAME = 'test action name';
+
+interface ActionProps {
+  hello: string;
+  my: number;
+  friend: boolean;
+}
+
+const actionProps: ActionProps = {
+  friend: true,
+  hello: 'some string',
+  my: 2,
+};
+
+interface GlobalContext {
+  secondHello: string;
+}
+
+describe('Create Parameterized Action', () => {
+  const actionExecutorMock = jest.fn();
+
+  const testAction = createParametarizedAction<GlobalContext, ActionProps>(
+    TEST_ACTION_NAME,
+    actionExecutorMock
+  );
+  const actionPayload = testAction(actionProps);
+
+  test('Action constructor should have correct props', async () => {
+    expect(actionPayload.name).toBe('test action name');
+    expect(actionPayload.params).toMatchObject(actionProps);
+  });
+
+  test('Action should be in ActionsBuffer', () => {
+    expect(Actions.isActionAlreadyExists(TEST_ACTION_NAME)).toBeTruthy();
+  });
+
+  test('Action executor should have correct props', async () => {
+    const actionExecutor = Actions.find(TEST_ACTION_NAME);
+
+    if (!actionExecutor) {
+      throw new Error(`There is no "${TEST_ACTION_NAME}" action`);
+    }
+
+    expect(actionExecutor.type).toBe('ParameterizedAction');
+    expect(actionExecutor.name).toBe(TEST_ACTION_NAME);
+
+    expect(typeof actionExecutor.do === typeof Function).toBe(true);
+
+    await actionExecutor.do(
+      {test: 1},
+      {test: 2} as unknown as MessageContext,
+      actionProps
+    );
+
+    expect(actionExecutorMock).toHaveBeenCalledWith(
+      {test: 1},
+      {test: 2},
+      actionProps
+    );
+  });
+
+  test('Dublicate action creation should throw an "This action already exists" error', () => {
+    const createDublicateAction = () =>
+      createAction(TEST_ACTION_NAME, () => {});
+
+    expect(createDublicateAction).toThrowErrorMatchingInlineSnapshot(
+      `"Action with name \\"test action name\\" already exist"`
+    );
+  });
+});
